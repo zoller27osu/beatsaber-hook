897e678645c3f85f9f82399b40931319855aa2c9
Adam ? <adamznow@gmail.com>
2019-11-11 01:29:37 -0800

    Inheritance added to dumps.json, mod-checks.hpp for useful macros

---
+++ shared/utils/mod-checks.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace mod
+{
+    // init_t is called in modloader::preload
+    using init_t = void() noexcept;
+    // constructor_t is called in modloader::load
+    using lib_main_t = void();
+    // preload_t is called in modloader::accept_unity_handle
+    using preload_t = void() noexcept;
+    // load_t is called in il2cpp_init
+    using load_t = void() noexcept;
+} // namespace mod
+
+#define MOD_FUNCTION_ASSERT(funcname) \
+    ::std::is_same_v<std::remove_reference_t<decltype(&funcname)>, ::mod::funcname##_t*>
+
+#define CHECK_MOD_FUNCTION(funcname) \
+    static_assert(MOD_FUNCTION_ASSERT(funcname), \
+    #funcname " either has the wrong signature, or does not exist!")
+
+#define CHECK_INIT CHECK_MOD_FUNCTION(init)
+#define CHECK_CTOR CHECK_MOD_FUNCTION(lib_main)
+#define CHECK_PRELOAD CHECK_MOD_FUNCTION(preload)
+#define CHECK_LOAD CHECK_MOD_FUNCTION(load)
