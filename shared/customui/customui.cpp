4f5d4dd4bd8354a8a3bc62ea6a16a93d56d8a751
Michael Zoller <mazoller94@gmail.com>
2020-03-19 16:48:59 -0700

    Formatting fixes for customui.cpp

--- shared/customui/customui.cpp
+++ shared/customui/customui.cpp
@@ -9,18 +9,15 @@
 using namespace il2cpp_utils;
 
 int CustomUI::TextObject::counter = 0;
-namespace CustomUI
-{
-    bool TextObject::create()
-    {
+namespace CustomUI {
+    bool TextObject::create() {
         // gameobj = new GameObject("CustomTextUI");
         log(DEBUG, "TextObject::create: Creating gameObject: %s (object number: %i)", name.data(), counter);
         gameObj = New(GetClassFromName("UnityEngine", "GameObject"), createcsstr(name.data()));
 
         // gameObj.SetActive(false);
         log(DEBUG, "TextObject::create: Setting gameObject.active to false");
-        if (!RunMethod(gameObj, "SetActive", false))
-        {
+        if (!RunMethod(gameObj, "SetActive", false)) {
             log(ERROR, "TextObject::create: Failed to set active to false");
             return false;
         }
@@ -28,10 +25,9 @@ namespace CustomUI
         // gameObj.AddComponent<TextMeshProUGUI>();
         log(DEBUG, "TextObject::create: Getting type of TMPro.TextMeshProUGUI");
         Il2CppObject *type_tmpugui = GetSystemType("TMPro", "TextMeshProUGUI");
-    
+
         log(DEBUG, "TextObject::create: Adding component TMPro.TextMeshProUGUI");
-        if (!RunMethod(&textMesh, gameObj, "AddComponent", type_tmpugui))
-        {
+        if (!RunMethod(&textMesh, gameObj, "AddComponent", type_tmpugui)) {
             log(ERROR, "TextObject::create: Failed to add Component TMPro.TextMeshProUGUI");
             return false;
         }
@@ -42,32 +38,27 @@ namespace CustomUI
         Array<Il2CppObject *> *allObjects;
 
         // Find Objects of type TMP_fontAsset
-        if (!RunMethod(&allObjects, "UnityEngine", "Resources", "FindObjectsOfTypeAll", type_fontasset))
-        {
+        if (!RunMethod(&allObjects, "UnityEngine", "Resources", "FindObjectsOfTypeAll", type_fontasset)) {
             // EXCEPTION
             log(ERROR, "TextObject::create: Failed to Find Objects of type TMP_fontAsset");
             return false;
         }
         int match = -1;
-        for (int i = 0; i < allObjects->Length(); i++)
-        {
+        for (int i = 0; i < allObjects->Length(); i++) {
             // Treat it as a UnityEngine.Object (which it is) and call the name getter
             Il2CppString *assetName;
-            if (!RunMethod(&assetName, allObjects->values[i], "get_name"))
-            {
+            if (!RunMethod(&assetName, allObjects->values[i], "get_name")) {
                 // EXCEPTION
                 log(ERROR, "TextObject::create: Failed to run get_name of assetName");
                 return false;
             }
-            if (strcmp(to_utf8(csstrtostr(assetName)).c_str(), "Teko-Medium SDF No Glow") == 0)
-            {
+            if (strcmp(to_utf8(csstrtostr(assetName)).c_str(), "Teko-Medium SDF No Glow") == 0) {
                 // Found matching asset
                 match = i;
                 break;
             }
         }
-        if (match == -1)
-        {
+        if (match == -1) {
             log(ERROR, "TextObject::create: Could not find matching TMP_FontAsset!");
             return false;
         }
@@ -75,16 +66,14 @@ namespace CustomUI
         // Instantiating the font
         log(DEBUG, "TextObject::create: Instantiating the font");
         Il2CppObject *font;
-        if (!RunMethod(&font, "UnityEngine", "Object", "Instantiate", allObjects->values[match]))
-        {
+        if (!RunMethod(&font, "UnityEngine", "Object", "Instantiate", allObjects->values[match])) {
             log(ERROR, "TextObject::create: Failed to Instantiate font!");
             return false;
         }
 
         // Setting the font
         log(DEBUG, "TextObject::create: Setting the font");
-        if (!RunMethod(textMesh, "set_font", font))
-        {
+        if (!RunMethod(textMesh, "set_font", font)) {
             log(ERROR, "TextObject::create: Failed to set font!");
             return false;
         }
@@ -92,82 +81,71 @@ namespace CustomUI
         // textMesh.rectTransform.SetParent(parent, false);
         log(DEBUG, "TextObject::create: Getting rectTransform");
         Il2CppObject *rectTransform;
-        if (!RunMethod(&rectTransform, textMesh, "get_rectTransform"))
-        {
+        if (!RunMethod(&rectTransform, textMesh, "get_rectTransform")) {
             log(ERROR, "TextObject::create: Failed to get rectTransform");
             return false;
         }
-    
+
         log(DEBUG, "TextObject::create: Setting Parent");
-        if (!RunMethod(rectTransform, "SetParent", parentTransform, false))
-        {
+        if (!RunMethod(rectTransform, "SetParent", parentTransform, false)) {
             log(ERROR, "TextObject::create: Failed to set parent!");
-            if (!parentTransform)
-            {
+            if (!parentTransform) {
                 log(ERROR, "TextObject::create: parentTransform is null! Fix it!");
             }
             return false;
         }
         // textMesh.text = text;
         log(DEBUG, "TextObject::create: Setting Text");
-        if (!RunMethod(textMesh, "set_text", createcsstr(text.data())))
-        {
+        if (!RunMethod(textMesh, "set_text", createcsstr(text.data()))) {
             log(ERROR, "TextObject::create: Failed to set text!");
             return false;
         }
 
         // textmesh.fontSize = fontSize;
         log(DEBUG, "TextObject::create: Setting fontSize");
-        if (!RunMethod(textMesh, "set_fontSize", fontSize))
-        {
+        if (!RunMethod(textMesh, "set_fontSize", fontSize)) {
             log(ERROR, "TextObject::create: Failed to set fontSize!");
             return false;
         }
 
         // textMesh.color = Color.white;
         log(DEBUG, "TextObject::create: Setting color");
-        if (!RunMethod(textMesh, "set_color", color))
-        {
+        if (!RunMethod(textMesh, "set_color", color)) {
             log(ERROR, "TextObject::create: Failed to set color!");
             return false;
         }
 
         // textMesh.rectTransform.anchorMin = anchorMin
         log(DEBUG, "TextObject::create: Setting anchorMin");
-        if (!RunMethod(rectTransform, "set_anchorMin", anchorMin))
-        {
+        if (!RunMethod(rectTransform, "set_anchorMin", anchorMin)) {
             log(ERROR, "TextObject::create: Failed to set anchorMin");
             return false;
         }
 
         // textMesh.rectTransform.anchorMax = anchorMax
         log(DEBUG, "TextObject::create: Setting anchorMax");
-        if (!RunMethod(rectTransform, "set_anchorMax", anchorMax))
-        {
+        if (!RunMethod(rectTransform, "set_anchorMax", anchorMax)) {
             log(ERROR, "TextObject::create: Failed to set anchorMax");
             return false;
         }
-    
+
         // textMesh.rectTransform.sizeDelta = sizeDelta
         log(DEBUG, "TextObject::create: Setting sizeDelta");
-        if (!RunMethod(rectTransform, "set_sizeDelta", sizeDelta))
-        {
+        if (!RunMethod(rectTransform, "set_sizeDelta", sizeDelta)) {
             log(ERROR, "TextObject::create: Failed to set sizeDelta");
             return false;
         }
 
         // textMesh.rectTransform.anchoredPosition = anchoredPosition
         log(DEBUG, "TextObject::create: Setting anchoredPosition");
-        if (!RunMethod(rectTransform, "set_anchoredPosition", anchoredPosition))
-        {
+        if (!RunMethod(rectTransform, "set_anchoredPosition", anchoredPosition)) {
             log(ERROR, "TextObject::create: failed to set anchoredPosition");
             return false;
         }
 
         // gameObj.SetActive(true);
         log(DEBUG, "TextObject::create: Setting gameObject active to true");
-        if (!RunMethod(gameObj, "SetActive", true))
-        {
+        if (!RunMethod(gameObj, "SetActive", true)) {
             log(ERROR, "TextObject::create: Failed to set active to true");
             return false;
         }
@@ -232,75 +210,62 @@ namespace CustomUI
         }
     }
 
-    bool RawImageObject::textureWebRequestComplete(RawImageObject* obj, Il2CppObject* asyncOp)
-    {
+    bool RawImageObject::textureWebRequestComplete(RawImageObject* obj, Il2CppObject* asyncOp) {
         // Notes: The field named "<webRequest>k__BackingField" on asyncOp is a pointer that should match obj->WWW
         log(DEBUG, "Entering textureWebRequestComplete!");
 
         Il2CppObject *rawImageRectTransform;
 
-        if (RunMethod(&obj->recievedTexture2D, il2cpp_utils::GetClassFromName("UnityEngine.Networking", "DownloadHandlerTexture"), "GetContent", obj->WWW))
-        {
+        if (RunMethod(&obj->recievedTexture2D, il2cpp_utils::GetClassFromName("UnityEngine.Networking", "DownloadHandlerTexture"), "GetContent", obj->WWW)) {
             obj->gameObj = New(GetClassFromName("UnityEngine", "GameObject"), createcsstr("RandomImage"));
-            if (!RunMethod(obj->gameObj, "SetActive", false))
-            {
+            if (!RunMethod(obj->gameObj, "SetActive", false)) {
                 log(ERROR, "Failed to set gameObj active to false");
             }
-            if (!RunMethod(&obj->rawImage, obj->gameObj, "AddComponent", GetSystemType("UnityEngine.UI", "RawImage")))
-            {
+            if (!RunMethod(&obj->rawImage, obj->gameObj, "AddComponent", GetSystemType("UnityEngine.UI", "RawImage"))) {
                 log(ERROR, "Failed to AddComponent rawImage");
             }
-            else if (!RunMethod(obj->rawImage, "set_texture", obj->recievedTexture2D))
-            {
+            else if (!RunMethod(obj->rawImage, "set_texture", obj->recievedTexture2D)) {
                 log(ERROR, "Failed to set recievedTexture2D");
             }
-            if (!RunMethod(&rawImageRectTransform, obj->rawImage, "get_rectTransform"))
-            {
+            if (!RunMethod(&rawImageRectTransform, obj->rawImage, "get_rectTransform")) {
                 log(ERROR, "Failed to get rectTransform");
             }
 
-            if (!obj->parentTransform)
-            {
+            if (!obj->parentTransform) {
                 log(ERROR, "RawImageObject::textureWebRequestComplete: obj->parentTransform is null! Fix it!");
             }
-            else if (!RunMethod(rawImageRectTransform, "SetParent", obj->parentTransform, false))
-            {
+            else if (!RunMethod(rawImageRectTransform, "SetParent", obj->parentTransform, false)) {
                 log(ERROR, "Failed to set Parent");
             }
 
             // textMesh.rectTransform.anchorMin = anchorMin
             log(DEBUG, "RawImageObject::textureWebRequestComplete: Setting anchorMin");
-            if (!RunMethod(rawImageRectTransform, "set_anchorMin", obj->anchorMin))
-            {
+            if (!RunMethod(rawImageRectTransform, "set_anchorMin", obj->anchorMin)) {
                 log(ERROR, "RawImageObject::textureWebRequestComplete: Failed to set anchorMin");
                 return false;
             }
 
             // textMesh.rectTransform.anchorMax = anchorMax
             log(DEBUG, "RawImageObject::textureWebRequestComplete: Setting anchorMax");
-            if (!RunMethod(rawImageRectTransform, "set_anchorMax", obj->anchorMax))
-            {
+            if (!RunMethod(rawImageRectTransform, "set_anchorMax", obj->anchorMax)) {
                 log(ERROR, "RawImageObject::textureWebRequestComplete: Failed to set anchorMax");
                 return false;
             }
 
             // textMesh.rectTransform.sizeDelta = sizeDelta
             log(DEBUG, "RawImageObject::textureWebRequestComplete: Setting sizeDelta");
-            if (!RunMethod(rawImageRectTransform, "set_sizeDelta", obj->sizeDelta))
-            {
+            if (!RunMethod(rawImageRectTransform, "set_sizeDelta", obj->sizeDelta)) {
                 log(ERROR, "RawImageObject::textureWebRequestComplete: Failed to set sizeDelta");
                 return false;
             }
 
             // textMesh.rectTransform.anchoredPosition = anchoredPosition
             log(DEBUG, "RawImageObject::textureWebRequestComplete: Setting anchoredPosition");
-            if (!RunMethod(rawImageRectTransform, "set_anchoredPosition", obj->anchoredPosition))
-            {
+            if (!RunMethod(rawImageRectTransform, "set_anchoredPosition", obj->anchoredPosition)) {
                 log(ERROR, "RawImageObject::textureWebRequestComplete: failed to set anchoredPosition");
                 return false;
             }
-            if (!RunMethod(obj->gameObj, "SetActive", true))
-            {
+            if (!RunMethod(obj->gameObj, "SetActive", true)) {
                 log(ERROR, "Failed to set gameObj active to true");
                 return false;
             }
@@ -313,8 +278,7 @@ namespace CustomUI
         return true;
     }
 
-    bool RawImageObject::create()
-    {
+    bool RawImageObject::create() {
         if (!RunMethod(&WWW, "UnityEngine.Networking", "UnityWebRequestTexture", "GetTexture", createcsstr(url))) {
             log(ERROR, "Failed to run UnityWebRequestTexture.GetTexture!");
             return false;
@@ -345,5 +309,4 @@ namespace CustomUI
 
         return true;
     }
-
 } // namespace CustomUI
