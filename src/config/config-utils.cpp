e780071c7001c1e0eedcf6516b8e68fb69821704
Adam ? <adamznow@gmail.com>
2020-08-04 15:27:53 -0700

    Use latest qpm, where `shared` is copied

--- src/config/config-utils.cpp
+++ src/config/config-utils.cpp
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
-#include "modloader/modloader.hpp"
+#include "modloader/shared/modloader.hpp"
 
 // CONFIG
 
--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -10,7 +10,7 @@
 #include <sstream>
 #include <unordered_set>
 #include "il2cpp-object-internals.h"
-#include "modloader/modloader.hpp"
+#include "modloader/shared/modloader.hpp"
 
 void safeAbort(const char* func, const char* file, int line) {
     #ifdef FILE_LOG
